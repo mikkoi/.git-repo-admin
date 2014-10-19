100637d46f8a297d14c4cad46aeafde897cd2035
Mikko Koivunalho <mikko.koivunalho@iki.fi>
2014-10-19 09:53:43 +0300

    Fixed changing dir issue.

--- .git-repo-admin/git-hooks.pl
+++ .git-repo-admin/git-hooks.pl
@@ -1,3 +1,5 @@
 #!/usr/bin/env perl
+use strict; use warnings;
+chdir $ARGV[0];
 use Git::Hooks;
 run_hook(-bash, @ARGV);
--- .git-repo-admin/git-hooks.sh
+++ .git-repo-admin/git-hooks.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
-cd /home/mikko/repos/repo.hooks/.git-repo-admin
-carton exec git-hooks.pl
+#!/bin/bash
+BARE_REPO_DIR=$(pwd)
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd ${DIR}
+carton exec ./git-hooks.sh ${BARE_REPO_DIR}
+
