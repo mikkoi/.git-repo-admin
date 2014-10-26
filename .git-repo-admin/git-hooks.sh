ecc2f26c269c2b40d95cf7f4f9de5e73302f57ff
Mikko Koivunalho <mikko.koivunalho@iki.fi>
2014-10-26 21:14:43 +0200

    Tweak: Execute 'git pull origin master' in the hooks repo.

--- .git-repo-admin/git-hooks.sh
+++ .git-repo-admin/git-hooks.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-echo "params:$@"
+
+#
+# Jumps to the admin directory and executes the perl script underneath
+# this Bash script.
+# The directory is "controlled" by plenv and Carton, forces perl 
+# to be desired version, not system perl.
+
+# Activate verbose if desired
+VERBOSE=0
+CONF_DBG=`git config --get githooks.edebug` && [ "${CONF_DBG}" = "1" ] && DEBUG=1
+if [ "$VERBOSE" = "1" ]; then echo "Parameters:$@"; fi
 BARE_REPO_DIR=$(pwd)
 HOOK_NAME=${BASH_SOURCE} # Actually "hooks/<hook name>"
-echo "HOOK_NAME=${HOOK_NAME}"
-#git config --list
+if [ "$VERBOSE" = "1" ]; then echo "HOOK_NAME=${HOOK_NAME}"; fi
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -11,10 +20,21 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 THIS_SCRIPT="${SOURCE}"
-echo "This script:${SOURCE}"
-
-
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd ${DIR}
-echo "Now in dir ${DIR}"
-exec carton exec ./git-hooks.pl ${BARE_REPO_DIR} ${HOOK_NAME} $@
+exec carton exec perl -x ${THIS_SCRIPT} ${BARE_REPO_DIR} ${HOOK_NAME} $@
+# *** End of Bash script ***
+
+# *** Start of Perl script
+#!/usr/bin/env perl
+use strict; use warnings;
+my $verbose = 0;
+$verbose = $ENV{'VERBOSE'} if defined $ENV{'VERBOSE'};
+my $central_repo_dir = shift @ARGV;
+my $hook_name = shift @ARGV;
+chdir $central_repo_dir;
+print "git-hooks.sh(pl) now in dir '$central_repo_dir'.\n" if ($verbose);
+use Git::Hooks;
+print "Executing: run_hook($hook_name, @ARGV)\n" if ($verbose);
+run_hook($hook_name, @ARGV);
+
--- .git-repo-admin/hooks.central/post-receive/update_hooks_repo.sh
+++ .git-repo-admin/hooks.central/post-receive/update_hooks_repo.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 BARE_REPO_DIR=$(pwd)
 echo "post-receive/update_hooks_repo.sh: Now in dir ${BARE_REPO_DIR}"
 cd ../repo.hooks
 echo "Now executing: git pull origin master"
 # git pull origin master
+
+#./test.sh < /dev/null > nohup.out 2>&1 &
+nohup "sleep 5; date; git pull origin master" < /dev/null >> ../update-hooks-repo.log 2>&1 &
 
