18ffcc39d0aee350d8e94f99c10953e2e6259e07
Mikko Koivunalho <mikko.koivunalho@iki.fi>
2014-10-26 18:10:18 +0200

    Change

--- .git-repo-admin/git-hooks.sh
+++ .git-repo-admin/git-hooks.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "params:$@"
 BARE_REPO_DIR=$(pwd)
+HOOK_NAME=${BASH_SOURCE} # Actually "hooks/<hook name>"
+echo "HOOK_NAME=${HOOK_NAME}"
+#git config --list
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
+THIS_SCRIPT="${SOURCE}"
+echo "This script:${SOURCE}"
+
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd ${DIR}
 echo "Now in dir ${DIR}"
-carton exec ./git-hooks.pl ${BARE_REPO_DIR}
+exec carton exec ./git-hooks.pl ${BARE_REPO_DIR} ${HOOK_NAME} $@
 
