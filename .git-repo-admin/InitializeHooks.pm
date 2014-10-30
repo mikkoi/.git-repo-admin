6d79c31b638d58e844814f2f28bb01bd6ff40baf
Mikko Koivunalho <mikko.koivunalho@iki.fi>
2014-10-30 17:24:51 +0200

    Tweak: initialize-hooks.sh works.

--- .git-repo-admin/InitializeHooks.pm
+++ .git-repo-admin/InitializeHooks.pm
@@ -23,42 +23,8 @@ use Sys::Hostname qw();
 use Data::Dumper qw(Dumper);
 
 # Constants
-###use constant GIT_HOOKS => qw(
-   ###pre-receive
-   ###pre-rebase
-   ###pre-commit
-   ###pre-auto-gc
-   ###pre-applypatch
-   ###post-update
-   ###post-rewrite
-   ###post-receive
-   ###post-merge
-   ###post-commit
-   ###post-checkout
-   ###post-applypatch
-   ###commit-msg
-   ###applypatch-msg
-   ###update
-   ###prepare-commit-msg
-###);
 
 # Some globals:
-#my $verbose = $ENV{'GIT_HOOKS_DEBUG'};
-#my $repo_admin_dirname = File::Spec->rel2abs(File::Spec->catfile(
-      #File::Spec->curdir() ));
-#my $repo_base_dirname = File::Spec->rel2abs(File::Spec->catfile(
-      #$repo_admin_dirname, File::Spec->updir() ));
-#my $git_hooks_filename = File::Spec->rel2abs(File::Spec->catfile(
-      #$repo_admin_dirname, 'git-hooks.sh'));
-#my $central_repo_config_filename = File::Spec->rel2abs(File::Spec->catfile(
-      #$repo_admin_dirname, 'hooks_config.central'));
-#my $local_repo_config_filename = File::Spec->rel2abs(File::Spec->catfile(
-      #$repo_admin_dirname, 'hooks_config.local'));
-#my $config_filename = File::Spec->catfile(
-      #File::Spec->updir(), '.git', 'config');
-#my $link_to_other_config_dirname = File::Spec->rel2abs(File::Spec->catfile(
-      #File::Spec->updir(), '.git'));
-#my $other_config_linkname = 'hooks_config';
 my $timestamp = (localtime)[5]+1900 . '-'
       . qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec )[(localtime time)[4]]
       . '-' . (localtime time)[3];
@@ -76,7 +42,7 @@ sub check_plenv {
    if(system('command -v plenv >/dev/null') ) {
       print "*" x 50, "\n";
       print "*" x 50, "\n";
-      print " *** You do not have 'plenv' in your system! ***\n";
+      print "$RED *** You do not have 'plenv' in your system! ***$NO_COLOUR\n";
       print " *** You must first install plenv. The easiest way to do that\n";
       print " *** is to install anyenv package:\n";
       print " *** 'git clone https://github.com/riywo/anyenv ~/.anyenv'\n";
@@ -148,10 +114,14 @@ sub get_bare_repo_dir {
 # 	path = config_hooks
 sub fix_git_config {
    my %params = @_;
+   print Dumper(\%params) if ($verbose);
    my $verbose = defined $params{'verbose'} ? $params{'verbose'} : 0;
    my $dry_run = defined $params{'dry_run'} ? $params{'dry_run'} : 0;
    my $repo_cfg_dir = defined $params{'repo_cfg_dir'} ? $params{'repo_cfg_dir'} : 0;
    my $hooks_cfg_linkname = defined $params{'hooks_cfg_linkname'} ? $params{'hooks_cfg_linkname'} : 0;
+   my $action = defined $params{'action'} ? $params{'action'} : 0;
+   die "Unknown action '$action'!" if ($action !~ /^(INSTALL|REMOVE)$/);
+
    my @config_rows;
    my $config_filename = File::Spec->rel2abs(File::Spec->catfile(
       $repo_cfg_dir, 'config' ));
@@ -163,11 +133,34 @@ sub fix_git_config {
       print "Existing config:$config_row.\n" if $verbose;
       $already_set = 1 if($config_row =~ /path[\s]*=[\s]*$hooks_cfg_linkname/);
    }
-   if( ! $already_set ) {
+   if($action eq 'INSTALL') {
+      print "Making a backup of the existing config and adding rows\n" if $verbose;
+      print "(to include the config file '$hooks_cfg_linkname').\n" if $verbose;
+      if(! $already_set) {
+         File::Copy::copy($config_filename, $config_bak_filename) unless $dry_run;
+         push @config_rows, "[include]\n" unless $dry_run;
+         push @config_rows, "\tpath = $hooks_cfg_linkname\n" unless $dry_run;
+      }
+      else {
+         print "Already set, no changes.\n";
+      }
+   }
+   else {
+      print "Removing rows from file 'config'.\n";
       print "Making a backup of the existing config and adding rows \n(to include the config file '$hooks_cfg_linkname').\n" if $verbose;
       File::Copy::copy($config_filename, $config_bak_filename) unless $dry_run;
-      push @config_rows, "[include]\n" unless $dry_run;
-      push @config_rows, "\tpath = $hooks_cfg_linkname\n" unless $dry_run;
+      for(my $i = 0; $i < @config_rows; $i++) {
+         my $config_row = $config_rows[$i];
+         print ":'$config_row'\n" if $verbose;
+         if($config_row =~ /path[\s]*=[\s]*$hooks_cfg_linkname/) {
+            print "Remove the following rows:\n:$config_rows[$i-1]\n:$config_rows[$i]\n" if $verbose;
+            my @removed = splice @config_rows, $i-1, 2 unless $dry_run;
+            print "Removed the following rows:\n";
+            print join "\n", @removed; print "\n";
+            $already_set = 0;
+            last;
+         }
+      }
    }
    return $already_set;
 }
@@ -189,19 +182,22 @@ sub link_file_to_other_config {
    my $repo_cfg_dir = defined $params{'repo_cfg_dir'} ? $params{'repo_cfg_dir'} : 0;
    my $hooks_cfg_linkname = defined $params{'hooks_cfg_linkname'} ? $params{'hooks_cfg_linkname'} : '';
    my $hooks_cfg_filename = defined $params{'hooks_cfg_filename'} ? $params{'hooks_cfg_filename'} : 0;
-   #my $bare_repo_path = defined $params{'bare_repo_path'} ? $params{'bare_repo_path'} : 0;
+   my $action = defined $params{'action'} ? $params{'action'} : 0;
 
-   my $link_filepath = File::Spec->catdir($repo_cfg_dir, $hooks_cfg_linkname);
-   my $link_to_filepath = File::Spec->catdir(File::Spec->curdir(), $hooks_cfg_filename);
+   my $link_filepath = File::Spec->rel2abs(File::Spec->catdir($repo_cfg_dir, $hooks_cfg_linkname));
+   my $link_to_filepath = File::Spec->rel2abs(File::Spec->catdir(File::Spec->curdir(), $hooks_cfg_filename));
    if( -e $link_filepath && ! -l $link_filepath ) {
-      die "The file '$link_filepath' already exists and it not a link. Aborting...";
+      die "The file '$link_filepath' already exists and it is not a link. Aborting...";
    }
    else {
       my @unlink_params = ($link_filepath);
       my @symlink_params = ($link_to_filepath, $link_filepath);
-      print "Execute: unlink ", (join ",", @unlink_params), " symlink ", (join ",", @symlink_params), "\n" if($verbose);
-      unlink $link_filepath if (! $dry_run);
-      symlink $link_to_filepath, $link_filepath if ( ! $dry_run);
+         print "Execute: unlink ", (join ",", @unlink_params), "\n" if($verbose);
+         unlink $link_filepath if (! $dry_run);
+      if($action eq 'INSTALL') {
+         print "Execute:", " symlink ", (join ",", @symlink_params), "\n" if($verbose);
+         symlink $link_to_filepath, $link_filepath if ( ! $dry_run);
+      }
    }
    return 1;
 }
@@ -212,15 +208,17 @@ sub setup_git_hooks {
    my $verbose = defined $params{'verbose'} ? $params{'verbose'} : 0;
    my $dry_run = defined $params{'dry_run'} ? $params{'dry_run'} : 0;
    my $hooks = defined $params{'hooks'} ? $params{'hooks'} : [ ];
-   #my $bare_repo_path = defined $params{'bare_repo_path'} ? $params{'bare_repo_path'} : 0;
    my $repo_cfg_dir = defined $params{'repo_cfg_dir'} ? $params{'repo_cfg_dir'} : 0;
+   my $action = defined $params{'action'} ? $params{'action'} : 0;
+   my $userhooks_dirname = defined $params{'userhooks_dirname'} ? $params{'userhooks_dirname'} : 0;
+
    my $link_to_filename = 'git-hooks.sh';
    my $link_dirpath = File::Spec->rel2abs(File::Spec->catfile( $repo_cfg_dir, 'hooks'));
    my $link_to_filepath = File::Spec->rel2abs(File::Spec->catfile(
          File::Spec->curdir(), $link_to_filename));
    foreach my $hook_filename ( @{$hooks} ) {
       my $link_filepath = File::Spec->rel2abs(File::Spec->catfile(
-            $repo_cfg_dir, $hook_filename));
+            $repo_cfg_dir, 'hooks', $hook_filename));
       print "Creating symlink from '$link_filepath' "
             . "to connect to Git::Hooks ($link_to_filepath).\n" if $verbose;
       if( -e $link_filepath && ! -l $link_filepath ) {
@@ -229,15 +227,18 @@ sub setup_git_hooks {
       else {
          my @unlink_params = ($link_filepath);
          my @symlink_params = ($link_to_filepath, $link_filepath);
-         print "Execute: unlink ", (join ",", @unlink_params), " symlink ", (join ",", @symlink_params), "\n" if($verbose);
+         print "Execute: unlink ", (join ",", @unlink_params), "\n" if($verbose);
          unlink $link_filepath unless $dry_run;
-         symlink $link_to_filepath, $link_filepath unless $dry_run;
+         if($action eq 'INSTALL') {
+            print "Execute: symlink ", (join ",", @symlink_params), "\n" if($verbose);
+            symlink $link_to_filepath, $link_filepath unless $dry_run;
+         }
       }
    }
-   $link_to_filepath = File::Spec->rel2abs(File::Spec->catfile(File::Spec->curdir(), 'hooks.d.local'));
+   $link_to_filepath = File::Spec->rel2abs(File::Spec->catfile(File::Spec->curdir(), $userhooks_dirname));
    my $link_filepath = File::Spec->rel2abs(File::Spec->catfile($repo_cfg_dir, 'hooks.d'));
 
-   print "Create link to directory 'hooks.d'. ";
+   print "Link to directory 'hooks.d'... ";
    print "(used by Git::Hooks for user hooks).\n";
    if( -e $link_filepath && ! -l $link_filepath ) {
       die "The file '$link_filepath' already exists and it not a link. Aborting...";
@@ -245,9 +246,12 @@ sub setup_git_hooks {
    else {
       my @unlink_params = ($link_filepath);
       my @symlink_params = ($link_to_filepath, $link_filepath);
-      print "Execute: unlink ", (join ",", @unlink_params), " symlink ", (join ",", @symlink_params), "\n" if($verbose);
+      print "Execute: unlink ", (join ",", @unlink_params), "\n" if($verbose);
       unlink $link_filepath if (! $dry_run);
-      symlink $link_to_filepath, $link_filepath if ( ! $dry_run);
+      if($action eq 'INSTALL') {
+         print "Execute: symlink ", (join ",", @symlink_params), "\n" if($verbose);
+         symlink $link_to_filepath, $link_filepath if ( ! $dry_run);
+      }
    }
    return 1;
 }
@@ -261,7 +265,11 @@ sub execute {
    my $hooks = defined $params{'hooks'} ? $params{'hooks'} : [ ];
    my $hooks_cfg_filename = defined $params{'hooks_cfg_filename'}
          ? $params{'hooks_cfg_filename'} : '';
+   my $userhooks_dirname = defined $params{'userhooks_dirname'} ? $params{'userhooks_dirname'} : [ ];
+   my $action = defined $params{'action'} ? $params{'action'} : 0;
+   die "Unknown action '$action'!" if ($action !~ /^(INSTALL|REMOVE)$/);
    print Dumper(\%params) if ($verbose);
+   print "Dry-run activated!\n" if ($dry_run);
    
    my $title = "Setup this Git repository";
    print "*" x ((length $title) + 8), "\n";
@@ -270,24 +278,27 @@ sub execute {
 
    if(! check_plenv() ) { exit 1; }
    install_prerequisites('verbose' => $verbose, 'dry_run' => $dry_run,
-         'perl_version' => $perl_version, 'perl_name' => $perl_name );
-   my $already_set = 1; #fix_git_config();
+         'perl_version' => $perl_version, 'perl_name' => $perl_name ) unless $action eq 'REMOVE';
+   #my $already_set = 1; #fix_git_config();
    #my $hooks_cfg_filename = 'config_hooks.central';
    my $hooks_cfg_linkname = 'config_hooks';
-   fix_git_config('verbose' => $verbose, 'dry_run' => $dry_run,
-         'repo_cfg_dir' => $repo_cfg_dir, 'hooks_cfg_linkname' => $hooks_cfg_linkname );
-   if(! $already_set) {
-      link_file_to_other_config('verbose' => $verbose, 'dry_run' => $dry_run,
-            'repo_cfg_dir' => $repo_cfg_dir,
-            'hooks_cfg_linkname' => $hooks_cfg_linkname, # config_hooks, normally
-            'hooks_cfg_filename' => $hooks_cfg_filename, # config_hooks.(central|local)
-            );
-      setup_git_hooks('verbose' => $verbose, 'dry_run' => $dry_run,
-           'hooks' => $hooks, );
+   my $already_set = fix_git_config('verbose' => $verbose, 'dry_run' => $dry_run,
+         'repo_cfg_dir' => $repo_cfg_dir, 'hooks_cfg_linkname' => $hooks_cfg_linkname,
+         'action' => $action, );
+   if($already_set) {
+      #print "Git config is already modified. Not skipping reestablishing file links.\n";
+      print "Git config is already modified. Let's check the links.\n";
    }
-   else {
-      print "Git config is already modified. Skipping reestablishing file links.\n";
-   }
+   link_file_to_other_config('verbose' => $verbose, 'dry_run' => $dry_run, 'action' => $action,
+         'repo_cfg_dir' => $repo_cfg_dir,
+         'hooks_cfg_linkname' => $hooks_cfg_linkname, # config_hooks, normally
+         'hooks_cfg_filename' => $hooks_cfg_filename, # config_hooks.(central|local)
+         );
+   setup_git_hooks('verbose' => $verbose, 'dry_run' => $dry_run, 'action' => $action,
+         'repo_cfg_dir' => $repo_cfg_dir,
+          'hooks' => $hooks, 
+          'userhooks_dirname' => $userhooks_dirname,
+         );
    return 0;
 }
 
---
+++ .git-repo-admin/initialize-hooks.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+#
+# Jumps to the admin directory and executes the perl script underneath
+# this Bash script.
+# Install hooks.
+# Parameters:
+# --dry-run, don't install, just show what would do.
+# --remove, instead of installing the hooks, remove them.
+# --central, normally you want to install the local hooks.
+#       Use "central" to install the central repo hooks.
+#
+# Activate verbose by setting the git config item githooks.debug to "1".
+#
+
+# Activate verbose if desired
+VERBOSE=0
+CONF_DBG=`git config --get githooks.debug` && [ "${CONF_DBG}" = "1" ] && VERBOSE=1
+if [ "$VERBOSE" = "1" ]; then echo "Parameters:$@"; fi
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+THIS_SCRIPT=`basename ${SOURCE}`
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+if [ "$VERBOSE" = "1" ]; then echo "DIR=${DIR}"; fi
+cd ${DIR}
+#DEBUG_FILE="${THIS_SCRIPT}_debug_$(date +%Y%m%d%H%M%S)"
+CARTON_CMD="carton exec perl -x ${THIS_SCRIPT} $@"
+if [ "$VERBOSE" = "1" ]; then echo "CARTON_CMD=${CARTON_CMD}"; fi
+export VERBOSE
+exec ${CARTON_CMD}
+
+# *** End of Bash script ***
+
+
+# *** Start of Perl script
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+my $verbose = 0;
+my $action = 'INSTALL';
+my $dry_run = 0;
+my $install_central = 0;
+# Cannot pass --verbose because Carton will snatch it before us!
+$verbose = $ENV{'VERBOSE'} if defined $ENV{'VERBOSE'};
+grep { if($_ =~ /^(--){0,1}remove$/i) { $action = 'REMOVE'; } } @ARGV;
+grep { if($_ =~ /^(--){0,1}dry-run$/i) { $dry_run = 1; } } @ARGV;
+grep { if($_ =~ /^(--){0,1}central$/i) { $install_central = 1; } } @ARGV;
+print "Verbose activated!\n" if $verbose;
+use InitializeHooks;
+my $hooks_cfg_filename;
+my $userhooks_dirname;
+my $repo_cfg_dir;
+my @GIT_HOOKS;
+if($install_central) {
+   @GIT_HOOKS = qw(
+      pre-receive
+      post-receive
+      update
+   );
+   $hooks_cfg_filename = 'config_hooks.local';
+   $userhooks_dirname = 'hooks.d.local';
+   $repo_cfg_dir = File::Spec->catfile( InitializeHooks::get_bare_repo_dir() );
+}
+else {
+   @GIT_HOOKS = qw(
+      pre-receive
+      pre-rebase
+      pre-commit
+      pre-auto-gc
+      pre-applypatch
+      post-update
+      post-rewrite
+      post-receive
+      post-merge
+      post-commit
+      post-checkout
+      post-applypatch
+      commit-msg
+      applypatch-msg
+      update
+      prepare-commit-msg
+   );
+   $hooks_cfg_filename = 'config_hooks.central';
+   $userhooks_dirname = 'hooks.d.central';
+   $repo_cfg_dir = File::Spec->catfile( File::Spec->updir(), '.git', 'config' );
+}
+InitializeHooks::execute('verbose' => $verbose,
+   'dry_run' => $dry_run,
+   'hooks' => \@GIT_HOOKS,
+   'hooks_cfg_filename' => $hooks_cfg_filename,
+   'userhooks_dirname' => $userhooks_dirname,
+   'repo_cfg_dir' => $repo_cfg_dir,
+   'action' => $action,
+   );
+exit 0;
+
